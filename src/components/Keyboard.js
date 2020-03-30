9b34c3ce5d286bc12b3cd85d63aca5b582e8ab35
dcmorales <dc.morales1992@gmail.com>
2020-03-30 12:36:11 -0700

    Added a second octave to keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import Key from './Key';
+import Octave from './Octave';
 
 export default function Keyboard() {
   return (
     <View style={styles.keyboard}>
-      <Key color="white" note="C" />
-      <Key color="black" note="Db" />
-      <Key color="white" note="D" />
-      <Key color="black" note="Eb" />
-      <Key color="white" note="E" />
-      <Key color="white" note="F" />
-      <Key color="black" note="Gb" />
-      <Key color="white" note="G" />
-      <Key color="black" note="Ab" />
-      <Key color="white" note="A" />
-      <Key color="black" note="Bb" />
-      <Key color="white" note="B" />
+      <Octave />
+      <Octave />
     </View>
   );
 }
---
+++ src/components/Octave.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import Key from './Key';
+
+export default function Octave() {
+  return (
+    <View style={styles.octave}>
+      <Key color="white" note="C" />
+      <Key color="black" note="Db" />
+      <Key color="white" note="D" />
+      <Key color="black" note="Eb" />
+      <Key color="white" note="E" />
+      <Key color="white" note="F" />
+      <Key color="black" note="Gb" />
+      <Key color="white" note="G" />
+      <Key color="black" note="Ab" />
+      <Key color="white" note="A" />
+      <Key color="black" note="Bb" />
+      <Key color="white" note="B" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  octave: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+});
