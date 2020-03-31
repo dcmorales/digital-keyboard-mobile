93a7f759e9863a0af2bdd8004ff684c16a958960
dcmorales <dc.morales1992@gmail.com>
2020-03-31 16:57:43 -0700

    Extracted hard coded data into new component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,14 @@
 import React, { useContext } from 'react';
 import { Picker, Text, View } from 'react-native';
 
-import { Context } from '../../context/SelectionContext';
+import Selections from './Selections';
 
 export default function Header() {
-  const { selectedValue, setSelectedValue } = useContext(Context);
   const { container } = styles;
-  const listName = 'instrument';
 
   return (
     <View style={container}>
-      <Text>{listName}</Text>
-      <Picker
-        selectedValue={selectedValue}
-        style={{ height: 50, width: 150 }}
-        onValueChange={itemValue => setSelectedValue(itemValue)}
-      >
-        <Picker.Item label="Piano" value="piano" />
-        <Picker.Item label="Cowbell" value="cowbell" />
-      </Picker>
+      <Selections listName="instrument" item1="piano" item2="cowbell" />
     </View>
   );
 }
---
+++ src/components/Header/Selections.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { Picker, Text, View } from 'react-native';
+
+import { Context } from '../../context/SelectionContext';
+
+export default function Selections({ listName, item1, item2 }) {
+  const { selectedValue, setSelectedValue } = useContext(Context);
+
+  return (
+    <View>
+      <Text>{listName}</Text>
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 50, width: 150 }}
+        onValueChange={itemValue => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label={`${item1}`} value={`${item1}`} />
+        <Picker.Item label={`${item2}`} value={`${item2}`} />
+      </Picker>
+    </View>
+  );
+}
