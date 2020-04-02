3514165528dba4539dfb8e4828d44370eaa242c9
dcmorales <dc.morales1992@gmail.com>
2020-04-01 17:29:59 -0700

    Added context to button

--- src/components/Header/PlayButton.js
+++ src/components/Header/PlayButton.js
@@ -1,13 +1,17 @@
 import React, { useContext } from 'react';
 import { Button } from 'react-native';
 
+import { Context } from '../../context/SelectionContext';
 import { playSound } from '../../helpers/playSound';
 
 export default function PlayButton() {
+  const { selectedValue } = useContext(Context);
+  const instrument = selectedValue;
+
   return (
     <Button
       title="Play Keys"
-      onPress={() => playSound.handleKeyPress('cowbell', 'C3')}
+      onPress={() => playSound.handleKeyPress(instrument, 'C3')}
     />
   );
 }
