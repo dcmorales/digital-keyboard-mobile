ae8cbf7d5ee49523dd4ad7bae68f56c3c2ca1748
dcmorales <dc.morales1992@gmail.com>
2020-04-16 18:03:16 -0700

    Added header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { Header as HeaderElement } from 'react-native-elements';
 
 import SettingsModal from './SettingsModal';
 
 export default function Header() {
+  const { headerContainer, title } = styles;
+
   return (
     <HeaderElement
-      centerComponent={{ text: 'Digital Keyboard', style: { color: '#fff' } }}
+      centerComponent={{ text: 'Digital Keyboard', style: title }}
       rightComponent={<SettingsModal />}
+      containerStyle={headerContainer}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    height: 60,
+    padding: 35,
+    backgroundColor: '#0059b2',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 30,
+    borderBottomWidth: 3,
+    borderColor: '#3399ff',
+    paddingHorizontal: 15,
+  },
+});
